96e3c4308b8c3d1ffbbbb802fd520f1433255de6
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-18 16:10:47 +0530

    feat: add sub

--- packages/lib/src/index.js
+++ packages/lib/src/index.js
@@ -6,3 +6,12 @@
 export async function add(x, y) {
   return x + y
 }
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @returns {Promise<number>}
+ */
+export async function sub(x, y) {
+  return x - y
+}
